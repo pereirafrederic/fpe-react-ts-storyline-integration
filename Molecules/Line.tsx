342ae1690bb976800a373f9d274cbcc7f96614e9
pereira frédéric <56920249+pereirafrederic-wip@users.noreply.github.com>
2020-01-06 09:35:04 +0100

    sauv1

--- Molecules/Line.tsx
+++ Molecules/Line.tsx
@@ -1,29 +1,17 @@
-import React, { PureComponent, ReactNode } from 'react'
-import {ILine} from './Iinterface'
-import Case from '../Atoms/Case'
-interface IState { }
+import React, { PureComponent, ReactNode } from "react";
+import { ILine } from "./Iinterface";
+import Case from "../Atoms/Case";
+interface IState {}
 interface IProps {
-  line : ILine
- }
-
-class Line extends PureComponent<IProps, IState> {
-
-
-
-  render() {
-
-    const {cases } = this.props.line
-
-const domCases = cases.map((case) => {
-  return <Case case={case} />
-});
-    const  {line} = this.props;
-    return <div className="Line">
-    {domCases}
-    </div>
-  }
-
-
+  line: ILine;
 }
 
-export default Line
+class Line extends PureComponent<IProps, IState> {
+  render() {
+    const { cases } = this.props.line;
+    const domCases = cases.map( case, index => (<Case />));
+    return<div className="Line">{domCases}</div>;
+  }
+}
+
+export default Line;
--- index.tsx
+++ index.tsx
@@ -1,42 +1,44 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Line from './Molecules/Line';
-import {IStory} from './Organisms/Iinterface'
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Line from "./Molecules/Line";
+import { IStory } from "./Organisms/Iinterface";
 
-import {EEtatLecture} from '../Atoms/Iinterface'
-import './style.css';
+import { EEtatLecture } from "./Atoms/Iinterface";
+import "./style.css";
 
-interface AppProps { }
+interface AppProps {}
 interface AppState {
-    story: IStory
+  story: IStory;
 }
 
 class App extends Component<AppProps, AppState> {
   constructor(props) {
     super(props);
     this.state = {
-    story :{
-      lines :[
-{ 
-  cases : [
-    {
-      id: 1,
-      etatLecture : EEtatLecture
-    }
-  ]
-}        
-      ]
-    }
+      story: {
+        lines: [
+          {
+            cases: [
+              {
+                id: 1,
+                etatLecture: EEtatLecture
+              }
+            ]
+          }
+        ]
+      }
     };
   }
 
   render() {
     return (
       <div>
-        {this.state.story.lines.map(line => <Line line={line} /> )}
+        {this.state.story.lines.map(line => (
+          <Line line={line} />
+        ))}
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
